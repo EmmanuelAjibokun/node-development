c82c7aab4c7d694a0fc3676612a3dafb91a44af1
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-06-26 15:12:46 +0100

    Lesson 7: setting up Middleware - app.use, app.all - use thirdparty middleware (cors) - use inbuilt middleware

--- server.js
+++ server.js
@@ -2,6 +2,46 @@ const express = require('express');
 const app = express()
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+
+//  custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.mysite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    OptionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// buildt-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+// Set Content Security Policy header
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "connect-src 'self' http://localhost:3500"
+//   );
+//   next();
+// });
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get(/^\/$|\/index(.html)?$/, (req, res) => {
     // res.sendFile('./views/index.html', {root: __dirname});
@@ -38,10 +78,19 @@ const three = (req, res, next) => {
 }
 app.get(/^\/chain(.html)?$/, [one, two, three]);
 
-
-app.get(/(.*)/, (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.use('/')
+app.all(/.*/, (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 })
+
+app.use(errorHandler)
 
 // listening for request, should always be at the end of server.js
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
