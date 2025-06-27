a3774a2902874288c20caf08c9240a6a7979a1e1
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-06-27 16:51:01 +0100

    Lesson10: Authentication Implement user registration and authentication features with bcrypt integration

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const data = {};
-data.employees = require('../../model/employees.json');
 const employeesController = require('../../controllers/employeesController')
 
 
