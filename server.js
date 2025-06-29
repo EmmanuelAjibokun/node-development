d7c5b286a8cc3c97d7c080fcb06064be319205b5
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-06-29 03:59:17 +0100

    Lesson 11: JWT Auth
    Implement JWT authentication with login, logout, and token refresh functionality; update dependencies and middleware for CORS and cookie handling.

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions')
 
 //  custom middleware logger
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
@@ -20,15 +27,22 @@ app.use(express.urlencoded({extended: false}));
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
-app.use('/register', require('./routes/api/register'));
-app.use('/auth', require('./routes/api/auth'));
 
 
 // app.use('/')
